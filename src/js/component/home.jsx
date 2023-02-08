29edcb83c4cbb9fd1746798fefffed3cff3542c0
ZeInacioCB <zeinaciocb@gmail.com>
2023-02-08 19:52:59 +0000

    styles and css initialised

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center list-group">
+			<h1>ToDo List for ToDay</h1>
+			<ul>
+				<li>Wake up in the morning</li>
+				<li>Make the world a better place</li>
+				<li>Prepare the marketing file</li>
+				<li>Get a way to be in the friday video call</li>
+			</ul>
 		</div>
 	);
 };
